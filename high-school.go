83cf80c704ed031abef9dfd17532ae26ca894b9d
Kyle Kung <kkung@klipfolio.com>
2017-11-05 17:12:30 -0500

    add a scalable way of adding nodes

--- high-school.go
+++ high-school.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	//memberlist "github.com/hashicorp/memberlist"
 )
 
@@ -15,15 +16,38 @@ func main(){
 	//	fmt.Printf("Member: %s %s\n", member.Name, member.Addr)
 	//}
 
-	channel := make(chan string)
-	go sendNode(channel)
-	go listenNode(channel)
+	channels := []chan string{
+		make(chan string),
+		make(chan string),
+	}
+
+	go node(channels, 0)
+	go node(channels, 1)
+
+
+	channels[0]<-"Hiyo"
+	//channel := make(chan string)
+	//go sendNode(channel)
+	//go listenNode(channel)
 
 	var input string
 	fmt.Scanln(&input)
 	fmt.Println("done")
 }
 
+
+func node (mailBoxes []chan string, index int) {
+	mailBox := mailBoxes[index]
+	for i := 0;i<100; i++ {
+		message:= <-mailBox
+		fmt.Print("Message Received: "+ message + "\n")
+		for i:= range mailBoxes {
+			if i != index {
+				mailBoxes[i] <- "Message From: " + strconv.Itoa(index) + " Message: " + message;
+			}
+		}
+	}
+}
 
 func sendNode (sending chan string) {
 	sending<-"Hello"
@@ -32,8 +56,6 @@ func sendNode (sending chan string) {
 func listenNode(listening chan string) {
 	fmt.Print(<-listening)
 }
-
-
 
 func printNumbers(from string, count int) {
 	for i := 0; i < count; i++ {
